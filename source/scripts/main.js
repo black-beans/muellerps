2c4927018bf9f18b9cde28b333681d949134aeb4
Michael Kessler <michi@netzpiraten.ch>
2013-12-11 16:09:49 +0100

    Randomize slideshow and better configuration.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,2 @@
+$('#featured').randomize();
 $(document).foundation();
--- source/scripts/plugins.js
+++ source/scripts/plugins.js
@@ -4,13 +4,26 @@
 
 // Avoid `console` errors in browsers that lack a console.
 if (!(window.console && console.log)) {
-    (function() {
-        var noop = function() {};
-        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
-        var length = methods.length;
-        var console = window.console = {};
-        while (length--) {
-            console[methods[length]] = noop;
-        }
-    }());
+  (function() {
+    var noop = function() {};
+    var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+    var length = methods.length;
+    var console = window.console = {};
+    while (length--) {
+      console[methods[length]] = noop;
+    }
+  }());
 }
+
+$.fn.randomize = function(selector){
+  var $elems = selector ? $(this).find(selector) : $(this).children(),
+  $parents = $elems.parent();
+
+  $parents.each(function(){
+    $(this).children(selector).sort(function(){
+      return Math.round(Math.random()) - 0.5;
+    }).remove().appendTo(this);
+  });
+
+  return this;
+};
